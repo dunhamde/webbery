a3974561a0ca308f98fb5350385c2a845a17b0aa
Derek Dunham <dunham.derek@gmail.com>
2018-06-15 17:59:03 -0700

    string interview qs

--- python/strings.py
+++ python/strings.py
@@ -1,3 +1,76 @@
+def oneaway(a, b):
+    lena = len(a)
+    lenb = len(b)
+    if abs(lena - lenb) > 1
+        return False
+    if lena < lenb:
+        for i in enumerate(a):
+            if a[i] != b[i]:
+                if a[i+1:] == b[i+1:]:
+                    return True
+                else if a[i:] == b[i+1:]:
+                    return True
+                else if a[i+1:] == b[i:]
+                    return True
+                break
+            
+
+
+def check_palindrome(string):
+    strmap = dict()
+    for c in string:
+        if c not in strmap.keys():
+            strmap[c] = 1
+        else:
+            strmap[c] = strmap[c] + 1
+    if len(string)%2 == 0:
+        print('even')
+        for key in strmap.keys():
+            if strmap[key] != 2:
+                return False
+        return True
+    else:
+        print('odd')
+        oneoff = False
+        for key in strmap.keys():
+            if strmap[key] != 2:
+                if strmap[key] != 1:
+                    return False
+                else:
+                    if oneoff == True:
+                        return False
+                    oneoff = True
+        return True
+
+def urlify(strarr, len):
+    print("array len: " + str(len))
+    print(strarr)
+    end = len - 1
+    foundword = False
+    for index in range(end, 0, -1):
+        if strarr[index] != ' ':
+            print('end: ' + str(end))
+            print('index: ' + str(index))
+
+            foundword = True
+            strarr[end] = strarr[index]
+            end = end - 1
+            print(strarr)
+        else:
+            if foundword:
+                print('end: ' + str(end))
+                print('index: ' + str(index))
+
+                strarr[end-2:end+1] = '%20'
+                end = end - 3
+                print(strarr)
+            
+def test_urlify():
+    url = ['M','r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ']
+    url2 = ['M','r',' ',' ','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ',' ',' ']
+    
+    urlify(url2, len(url2))
+    
 def IsPerm(s1, perm):
     dicto = {}
     if len(s1) != len(perm):
@@ -22,7 +95,13 @@ def IsPerm(s1, perm):
     print("is perm")
     return True
 
-if IsPerm("catkdogg","ggotdac"):
-    print("this worked")
+def test_isperm():
+    if IsPerm("catkdogg","ggotdac"):
+        print("perm found")
+    else:
+        print("not a perm!")
+
+if not check_palindrome('derekrk'):
+    print('not pal')
 else:
-    print("this worked too!")
+    print('palindrome')
